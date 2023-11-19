7987ec3c1132fb7bb0344ee167fb903cea31dce3
dawn_lch <chung4111@naver.com>
2023-11-19 14:42:34 +0900

    231119

--- init.cpp
+++ init.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 void Init::show() {
     int select;
-    do {
+    
         cout << "1. 포장해서 갈래요" << endl;
         cout << "2. 매장에서 먹어요" << endl;
-        cin >> select;
+     do {   
+         cin >> select;
 
     } while (select != 1 && select != 2);
 }
--- menu.cpp
+++ menu.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include "header.h"
 using namespace std;
-int Menu::show(int &a) {
+int Menu::show(int &price) {
    
-    do {
         cout << "=====================================" << endl;
         cout << "커피" << endl;
         cout << "1. 아메리카노 - 1500원" << endl;
@@ -25,28 +24,31 @@ int Menu::show(int &a) {
         cout << "10. 결제하기" << endl;
         cout << "=====================================" << endl;
         cout << endl;
+     do {
         cin >> select;
 
         if (select == 10 && Menu_count == 0) {
             cout << "다시 입력하시오." << endl;
         }
+        else if (0 < select && select < 11)
+            Menu_count += 1;//주문한 메뉴 개수
         else
-            Menu_count += 1;
-    } while (select > 10 || select < 0 || Menu_count == 0);
+            Menu_count = 0;
+    } while (select > 10 || select < 0 || Menu_count == 0); // 범위밖의 숫자, 메뉴가 0일 때 다시수행
 
 
     if (select == 1 || select == 2 || select == 3) {
-        if (select == 1)a += 1500;
-        if (select == 2)a += 2900;
+        if (select == 1)price += 1500;
+        if (select == 2)price += 2900;
 
-        if (select == 3)a += 3300;
-        return a; //커피 옵션
+        if (select == 3)price += 3300;
+        return price; //커피 옵션
     }
     else if (select == 4 || select == 5 || select == 6) {
-        if (select == 4)a += 2000;
-        if (select == 5)a += 4000;
-        if (select == 6)a += 4000;
-        return a;//티/스무디 옵션
+        if (select == 4)price += 2000;
+        if (select == 5)price += 4000;
+        if (select == 6)price += 4000;
+        return price;//티/스무디 옵션
     }
     else if (select == 10) {
         return 2;//결제하기로
@@ -62,23 +64,24 @@ void Menu::option(int option[][10]) {
     int option_choose; // 세부 옵션 선택
 
    
-    do {
+    
         cout << "===========================================" << endl;
         cout << "                1. ICE" << endl;
         cout << "                2. HOT" << endl;
         cout << "===========================================" << endl;
+    do {
         cin >> temp;
     } while (temp!=1&&temp!=2);
 
     //여기까지 핫/ 아이스 결정
     //이제부터 무료, 유료 옵션 결정
-    do {
-        int repeat = 0;
+    int repeat = 0;
     cout << "===========================================" << endl;
     cout << "                1. 무료옵션" << endl;
     cout << "                2. 유료옵션" << endl;
     cout << "                3. 선택완료" << endl;
     cout << "===========================================" << endl;
+    do {
     cin >> option_select;
     //무료옵션인 경우
     
@@ -130,27 +133,27 @@ void Menu::option(int option[][10]) {
             }
             cin >> option_choose;
             if (option_choose == 1) {
-                option[1][option_choose] = 1;
+                option[1][option_choose] = 500;
                 cout << "바닐라 시럽이 추가되었습니다." << endl;
             }
             else if (option_choose == 2) {
-                option[1][option_choose] = 1;
+                option[1][option_choose] = 500;
                 cout << "헤이즐넛 시럽이 추가되었습니다." << endl;
             }
             else if (option_choose == 3) {
-                option[1][option_choose] = 1;
+                option[1][option_choose] = 500;
                 cout << "휘핑크림이 추가되었습니다." << endl;
             }
             else if (option_choose == 4) {
-                option[1][option_choose] = 1;
+                option[1][option_choose] = 500;
                 cout << "샷 추가되었습니다." << endl;
             }
             else if (option_choose == 5) {
-                option[1][option_choose] = 1;
+                option[1][option_choose] = 700;
                 cout << "펄 추가되었습니다." << endl;
             }
             else if (option_choose == 6) {
-                option[1][option_choose] = 1;
+                option[1][option_choose] = 500;
                 cout << "우유가 추가되었습니다." << endl;
             }
             else if (option_choose == 7) {
@@ -161,15 +164,3 @@ void Menu::option(int option[][10]) {
         }
     } while (option_select != 3);
 }
-
-//void Menu::option2() {
-//    int temp;
-//    cout << "1. ICE" << endl;
-//    cout << "2. HOT" << endl;
-//    do {
-//        cin >> temp;
-//    } while (temp != 1 && temp != 2);
-//    cout << "1. 휘핑크림 - 500원" << endl;
-//    cout << "2. 사이즈업 - 1000원" << endl;
-//    cout << "3. 우유추가 - 500원" << endl;
-//}
