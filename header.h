cc202244e8e58d13bf6c95e6f1951ae8b242926d
dawn_lch <chung4111@naver.com>
2023-11-24 00:48:41 +0900

    COMPLETE

--- header.h
+++ header.h
@@ -8,19 +8,15 @@ public:
 class Menu {
 public:
     int Menu_count = 0; //Menu의 출력횟수를 count
-    int select=0;
+    int select = 0;
     int show(int& price);//메뉴판 출력
-    int option(int option[][10]);//무료,유료 옵션
+    void option(int option[][10]);//무료,유료 옵션
 };
-class Receipt: public Menu {
+class Receipt : public Menu {
 public:
-    int menu_select = 0;
+    int menu_select;
     void name(int select_menu);
-    void option_name(int a);
     int price_total(int& price, int option[][10]);
-    int price_main(int& price);
-    int price_option(int& select_option);
-    void receipt_show(int a[], int b[], int c[], int d[]);
-    
+    void receipt_show(int a[], int b[]);
 };
 #endif
--- receipt.cpp
+++ receipt.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void Receipt::name(int select_menu) {
     switch (select_menu) {
     case 1:
-        cout<< "아메리카노 ";
+        cout << "아메리카노 ";
         break;
     case 2:
         cout << "카페라떼 ";
@@ -34,30 +34,6 @@ void Receipt::name(int select_menu) {
 
     }
 }
-
-void Receipt::option_name(int select_option) {
-    switch (select_option) {
-    case 1:
-        cout << "바닐라 시럽 ";
-        break;
-    case 2:
-        cout << "헤이즐넛 시럽 ";
-        break;
-    case 3:
-        cout << "휘핑크림 ";
-        break;
-    case 4:
-        cout << "샷추가 ";
-        break;
-    case 5:
-        cout << "펄추가 ";
-        break;
-    case 6:
-        cout << "우유 추가 ";
-        break;
-
-    }
-}
 int Receipt::price_total(int& price, int option[][10])
 {
     for (int i = 1; i < 7; i++) {   //0행(무료옵션)은 무료옵션이므로 1행(유료옵션)만 price에 합산한다.
@@ -65,51 +41,7 @@ int Receipt::price_total(int& price, int option[][10])
     }
     return price;
 }
-
-
-int Receipt::price_main(int& price) {  //개별 메인메뉴에 대한 가격 출력
-    switch (price) {
-    case 1:
-        return 1500;
-    case 2:
-        return 2900;
-    case 3:
-        return 3300;
-    case 4:
-        return 4000;
-    case 5:;
-        return 4000;
-    case 6:
-        return 4000;
-    case 7:
-        return 5000;
-    case 8:
-        return 5000;
-    case 9:
-        return 2200;
-    }
-}
-
-int Receipt::price_option(int& select_option) {  //옵션에 대한 가격 출력
-    switch (select_option) {
-    case 1:
-        return 500;
-    case 2:
-        return 500;
-    case 3:
-        return 500;
-    case 4:
-        return 500;
-    case 5:;
-        return 700;
-    case 6:
-        return 500;
-    case -1:     //선택완료
-        return 0;
-    }
-}
-
-void Receipt::receipt_show(int sum_menu[],int main_price[], int select_option[], int price_option[]) {
+void Receipt::receipt_show(int sum_menu[], int sum_price[]) {
     int total = 0;
     int enter = 0;
     cout << "                  [영 수 증]                " << endl;
@@ -118,27 +50,19 @@ void Receipt::receipt_show(int sum_menu[],int main_price[], int select_option[],
     cout << "[대표자] 홍상우 , 이충헌" << endl;
     cout << "===========================================" << endl;
     cout << " 상  품  명                          금 액" << endl;
-
- 
-    //메인메뉴 출력
-    for (int i = 0; i < 10; i++) {  
-        if (sum_menu[i] != 0 && main_price[i] != 0) {
-            name(sum_menu[i]);     
-            cout << "                             " << main_price[i] << endl;                    
-            //total += main_price[i];
-        }             
-    }
-    
-    //옵션 출력
-    for (int i = 0; i < 10; i++) {        
-        if (select_option[i] != -1 || price_option[i] != 0) {
-            option_name(select_option[i]);
-            cout << "                            " << price_option[i] << endl;
-            total += price_option[i];
+    for (int i = 0; i < 10; i++) {
+        if (sum_menu[i] != 0 && sum_price[i] != 0) {
+            name(sum_menu[i]);
+            cout << "                           " << sum_price[i] << endl;
+            total += sum_price[i];
+        }
+        else {
+            enter++;
         }
     }
-    for (int j = 0; j < 5; j++) {
+    for (int j = 0; j < enter; j++) {
         cout << endl;
-    }   
-   
+    }
+    cout << "===========================================" << endl;
+    cout << "합 계 금 액                          " << total << endl;
 }
